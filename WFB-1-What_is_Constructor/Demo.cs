96874b7fc40f9a52bc09fa5b23a20d7d9c56d6c9
Tung <93968563+tungtd34511@users.noreply.github.com>
2023-05-02 17:20:47 +0700

    Add project files.

---
+++ WFB-1-What_is_Constructor/Demo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFB_1_What_is_Constructor
+{
+    public class Demo
+    {
+        private int _count;
+
+        public Demo(int count)
+        {
+            _count = count;
+            Console.Write(Count);
+        }
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+    }
+}
---
+++ WFB-1-What_is_Constructor/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WFB_1_What_is_Constructor;
+
+var demo1 = new Demo(111);
